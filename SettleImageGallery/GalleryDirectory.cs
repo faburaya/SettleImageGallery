b2564f1425c2439738f3d06fc00365eafc9f299f
Felipe Aburaya <felipe.aburaya@hotmail.com>
2020-10-18 19:51:06 +0200

    Etwa Kernlogik für Verlegung von Dateien ist geschrieben

--- SettleImageGallery/GalleryDirectory.cs
+++ SettleImageGallery/GalleryDirectory.cs
@@ -1,13 +1,40 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SettleImageGallery
 {
-    internal class GalleryDirectory
+    public class GalleryDirectory
     {
-        public GalleryDirectory(string directoryPath)
+        private readonly FileSystemUtils.DirectoryNodeInfo _directoryTree;
+
+        public GalleryDirectory(FileSystemUtils.DirectoryNodeInfo directoryTree)
         {
+            this._directoryTree = directoryTree;
+        }
+
+        private static List<(string fromPath, string toNewName)>
+        PlanFileRenamings(FileSystemUtils.DirectoryNodeInfo dirInfoTree,
+                          string prefix)
+        {
+            if (dirInfoTree.FileNames.Any() && dirInfoTree.Subdirectories.Any())
+            {
+                throw new ApplicationException($"Ein Ordner enthält gleichzeitig Dateien und Ordner, was unmöglich macht, zu bestimmen, wo die Dateien eingeordnet werden können. Bitte verlegen Sie sie unter einem Ordner, der nur Dateien enthält. - {dirInfoTree.FullPath}");
+            }
+
+            var movements = new List<(string fromPath, string toPath)>();
+
+            foreach (var subdir in dirInfoTree.Subdirectories)
+            {
+
+            }
+
+            foreach (string fileName in dirInfoTree.FileNames)
+            {
+
+            }
+
+            return movements;
         }
     }
 }
