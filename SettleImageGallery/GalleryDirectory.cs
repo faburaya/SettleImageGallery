b401a7457e7bf1583b0a803b937c9090c7f3c740
Felipe Aburaya <felipe.aburaya@hotmail.com>
2020-10-20 22:46:40 +0200

    Erster Entwurf der Logik, die das Umbennen aller Dateien plant.

--- SettleImageGallery/GalleryDirectory.cs
+++ SettleImageGallery/GalleryDirectory.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.IO;
 
 namespace SettleImageGallery
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class GalleryDirectory
     {
         private readonly FileSystemUtils.DirectoryNodeInfo _directoryTree;
@@ -13,28 +18,65 @@ namespace SettleImageGallery
             this._directoryTree = directoryTree;
         }
 
+        /// <summary>
+        /// Sucht die Diagramm (DFS), um die Umbenennungen der Dateien zu planen.
+        /// </summary>
+        /// <param name="dirInfoTree">Die Diagramm des Dateisystems unter einem Ordner.</param>
+        /// <param name="prefix">
+        /// Das Präfix für den obersten Ordner in der angegebenen Diagram,
+        /// das für alle Dateien unmittelbar darunter angewandt werden muss.
+        /// </param>
+        /// <returns>Eine Planung mit allen Umbenennungen, die ausgeführt werden müssen. </returns>
         private static List<(string fromPath, string toNewName)>
         PlanFileRenamings(FileSystemUtils.DirectoryNodeInfo dirInfoTree,
                           string prefix)
         {
             if (dirInfoTree.FileNames.Any() && dirInfoTree.Subdirectories.Any())
             {
-                throw new ApplicationException($"Ein Ordner enthält gleichzeitig Dateien und Ordner, was unmöglich macht, zu bestimmen, wo die Dateien eingeordnet werden können. Bitte verlegen Sie sie unter einem Ordner, der nur Dateien enthält. - {dirInfoTree.FullPath}");
+                throw new ApplicationException($"Ein Ordner enthält gleichzeitig Dateien und Ordner, was unmöglich macht, zu bestimmen, wo die Dateien eingeordnet werden können. Bitte verlegen Sie sie unter einem Ordner, der nur Dateien enthält. - \"{dirInfoTree.FullPath}\"");
             }
 
-            var movements = new List<(string fromPath, string toPath)>();
+            var renamings = new List<(string fromPath, string toPath)>();
 
-            foreach (var subdir in dirInfoTree.Subdirectories)
+            int fileCount = dirInfoTree.FileNames.Count();
+            if (fileCount > 0) // nur Dateien:
             {
+                if (fileCount > ushort.MaxValue)
+                {
+                    throw new ApplicationException($"Die Anzahl von Dateien im Ordner ist schlicht zu groß. - \"{dirInfoTree.FullPath}\" enthält {fileCount} Dateien");
+                }
 
+                var nameGenerator = new NamingUtils.NameGenerator();
+                string nameRoot = nameGenerator.GetRandomWord();
+                string[] suffixes = NamingUtils.NameGenerator.CreateOrderedListOfPrintedNumbers((ushort)fileCount);
+
+                int idx = 0;
+                foreach (string fileName in dirInfoTree.FileNames)
+                {
+                    string fromPath = Path.Join(dirInfoTree.FullPath, fileName);
+                    string toNewName = $"{prefix}_{nameRoot}_{suffixes[idx++]}.{Path.GetExtension(fileName)}";
+                    renamings.Add((fromPath, toNewName));
+                }
             }
-
-            foreach (string fileName in dirInfoTree.FileNames)
+            else // nur Ordner:
             {
+                int subdirCount = dirInfoTree.Subdirectories.Count();
+                if (subdirCount > ushort.MaxValue)
+                {
+                    throw new ApplicationException($"Die Anzahl von Ordner ist schlicht zu groß. - \"{dirInfoTree.FullPath}\" enthält {subdirCount} Ordner");
+                }
 
+                string[] indices = NamingUtils.NameGenerator.CreateOrderedListOfPrintedNumbers((ushort)subdirCount);
+
+                int idx = 0;
+                foreach (var subdir in dirInfoTree.Subdirectories)
+                {
+                    string newPrefix = $"{prefix}_{indices[idx++]}";
+                    renamings.AddRange(PlanFileRenamings(subdir, newPrefix));
+                }
             }
 
-            return movements;
+            return renamings;
         }
     }
 }
