93d271b5d6e94cb02dd66d34dbc774e4124c9816
Felipe Aburaya <felipe.aburaya@hotmail.com>
2020-10-22 01:19:22 +0200

    Neue Schnittstelle für Vermittlung des Zugangs auf das Dateisystem wurde eingerichtet.

---
+++ FileSystemUtils/FileSystemAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystemUtils
+{
+    public class FileSystemAccess : IFileSystemAccess
+    {
+        public bool MoveFile(string fromPath, string toPath)
+        {
+            try
+            {
+                new FileInfo(fromPath).MoveTo(toPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"FEHLER: Datei \"{fromPath}\" konnte nicht an \"{toPath}\" verschoben werden: {ex.Message}");
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ FileSystemUtils/IFileSystemAccess.cs
@@ -0,0 +1,11 @@
+﻿namespace FileSystemUtils
+{
+    /// <summary>
+    /// Vermittelt den Zugriff auf das Dateisystem, sodass man ihn durch ein Mock
+    /// ersetzen darf und Teste durchführt, ohne tatsächliche Dateien zu berühren.
+    /// </summary>
+    public interface IFileSystemAccess
+    {
+        bool MoveFile(string fromPath, string toPath);
+    }
+}
