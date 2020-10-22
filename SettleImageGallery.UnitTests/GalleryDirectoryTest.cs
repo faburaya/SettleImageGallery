4396d61f81c82d47dc925f7ee69ab170d73a625d
Felipe Aburaya <felipe.aburaya@hotmail.com>
2020-10-22 09:16:30 +0200

    Neues Projekt zum Testen des GallerDirektory-Klasse

---
+++ SettleImageGallery.UnitTests/GalleryDirectoryTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SettleImageGallery.UnitTests
+{
+    public class GalleryDirectoryTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
