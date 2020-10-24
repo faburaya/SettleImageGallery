ad72cd2639534a29f87c4e62e63b8e48cfc6273e
Felipe Aburaya <felipe.aburaya@hotmail.com>
2020-10-25 00:59:02 +0200

    Test für GalleryDirectory ist abgeschlossen.

--- SettleImageGallery.UnitTests/GalleryDirectoryTest.cs
+++ SettleImageGallery.UnitTests/GalleryDirectoryTest.cs
@@ -49,7 +49,26 @@ namespace SettleImageGallery.UnitTests
             var galleryDirectory = new GalleryDirectory(fileSystemAccessMock.Object);
             galleryDirectory.MoveAllImagesToFlatOrder(directory);
 
-            VerifyCallsTo(fileSystemAccessMock, dirPath, fileNames, $"^\\w+_\\d+\\.{_fileExt}$");
+            VerifyCallsTo(fileSystemAccessMock, dirPath, directory, $"^\\w+_\\d+\\.{_fileExt}$");
+        }
+
+        [Fact]
+        public void MoveAllImagesToFlatOrder_WhenSubdirectoriesPresent_ThenUsePrefix()
+        {
+            string dirPath = AsCrossPlatformPath("./home/Galerie");
+            string[] fileNames = CreateSomeFileNames();
+
+            DirectoryNodeInfo[] subdirs = {
+                new DirectoryNodeInfo(Path.Join(dirPath, "A"), null, fileNames),
+                new DirectoryNodeInfo(Path.Join(dirPath, "B"), null, fileNames),
+            };
+            var directory = new DirectoryNodeInfo(dirPath, subdirs, null);
+
+            Mock<IFileSystemAccess> fileSystemAccessMock = SetupMockForFileSystemAccess();
+            var galleryDirectory = new GalleryDirectory(fileSystemAccessMock.Object);
+            galleryDirectory.MoveAllImagesToFlatOrder(directory);
+
+            VerifyCallsTo(fileSystemAccessMock, dirPath, directory, $"^\\d+_\\w+_\\d+\\.{_fileExt}$");
         }
 
         private static Mock<IFileSystemAccess> SetupMockForFileSystemAccess()
@@ -62,14 +81,26 @@ namespace SettleImageGallery.UnitTests
             return mock;
         }
 
+        /// <summary>
+        /// Überprüft die Aufrufe für <see cref="IFileSystemAccess.MoveFile(string, string)"/>.
+        /// </summary>
+        /// <param name="mock">Das Mock-Objekt, das aufgerufen wird.</param>
+        /// <param name="destDirPath">Das Verzeichnis, worauf die Dateien verschoben werden müssen.</param>
+        /// <param name="directory">Stellt den Ordner der Galerie dar.</param>
+        /// <param name="regex">Der "Regex", dem die neuen Namen der Dateien entsprechen müssen.</param>
         private static void VerifyCallsTo(Mock<IFileSystemAccess> mock,
                                           string destDirPath,
-                                          IEnumerable<string> fileNames,
+                                          DirectoryNodeInfo directory,
                                           string regex)
         {
-            foreach (string fileName in fileNames)
+            foreach (DirectoryNodeInfo subdir in directory.Subdirectories)
             {
-                string fromPath = Path.Join(destDirPath, fileName);
+                VerifyCallsTo(mock, destDirPath, subdir, regex);
+            }
+
+            foreach (string fileName in directory.FileNames)
+            {
+                string fromPath = Path.Join(directory.FullPath, fileName);
                 mock.Verify(
                     obj => obj.MoveFile(
                         It.Is<string>(val => val == fromPath),
